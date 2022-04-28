52d24ef750bbda9f8d46d5488ad1881c9c96f766
brunoAluisoh <brunoaluisioh@gmail.com>
2022-04-27 21:07:30 -0300

    subindo exercicio realizado aula js 02

---
+++ js/Aula 02_Console_Variáveis_Operadores/exercicioSala.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+const nome = 'Bruno';
+const altura = 1.82;
+const peso = 60;
+console.log(nome)
+console.log(altura)
+console.log(peso)
+
+let imc = peso/(altura**2);
+// Template String
+const hello = `Meu nome é ${nome} e eu tenho ${altura} de altura e ${peso} de peso, meu IMC é ${imc}`;
+console.log(hello);
+
+
+
