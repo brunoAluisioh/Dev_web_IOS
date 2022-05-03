476089741457d6944660451befd04d9517ba1c6d
brunoAluisioh <brunoaluisioh@gmail.com>
2022-05-03 16:07:12 -0300

    subindo exercicio Aula Objetos_Funcoes_Eventos

---
+++ js/Aula 03_objetos_Funcoes_Eventos/mainScript.js
@@ -0,0 +1,22 @@
+//Estrutura simples
+function AlertCookie () {
+    alert('Você quer um cookie?')
+    console.log('Você quer um cookie?')
+}
+
+//Criando Arrow Function 
+const AlertSucesso = () => {
+    alert('Mensagem gerada com sucesso')
+    console.log('Mensagem gerada com sucesso')
+}
+
+//Criando Objetos
+const notebook = {
+    marca: 'Apple',
+    linha: 'Mac Book Pro',
+    tela: 'Retina',
+    memóriaRam: '8GB',
+    leitorBiométrico: 'true',
+    valor: '11.996,99'
+}
+console.log (notebook);
