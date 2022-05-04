2f94b8cee70c088387af392cd4b72980d7f4867f
brunoAluisioh <brunoaluisioh@gmail.com>
2022-05-04 11:22:04 -0300

    subindo exercicio realizado Aula 04_Arrays

--- js/Aula 04_Arrays/js/script.js
+++ js/Aula 04_Arrays/js/script.js
@@ -1,6 +1,11 @@
 //Arrays unidimensional
-const alunos = ['Bruno', 'Hercules', 'Davi', 'Joao', 'Daniel'];
+const alunos = ['Bruno', 'Julia', 'Davi', 'Joao', 'Daniel'];
 console.log(alunos);
 
 //Arrays bidimensionais
-
+const info = [
+    (nota = ['5', '6', '8', '9', '7']),
+    (ano = ['1998', '2000', '2002', '2001', '2003']),
+    (turma = ['Ads', 'Ads', 'Ads', 'Ads', 'Ads']),
+];
+console.log(info);
