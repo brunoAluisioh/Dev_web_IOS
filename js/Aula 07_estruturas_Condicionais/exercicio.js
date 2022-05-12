c4d564f1d976cd64fb1de85f1d69437b5e507ac3
brunoAluisioh <brunoaluisioh@gmail.com>
2022-05-11 21:51:15 -0300

    subindo Aula 07 Estruturas_Condicionais

---
+++ js/Aula 07_estruturas_Condicionais/exercicio.js
@@ -0,0 +1,34 @@
+let colaboradores = [
+    {
+        nome: 'Bruno',
+        salario: 5000,
+        vinculo: 'CLT'
+    },
+    {
+        nome: 'Brenno',
+        salario: 3000,
+        vinculo: 'PJ',
+    },
+    {
+        nome: 'Weverton',
+        salario: 4000,
+        vinculo: 'CLT',
+    },
+    {
+        nome: 'Julia',
+        salario: 6000,
+        vinculo: 'PJ',
+    },
+    {
+        nome: 'Mayara',
+        salario: 7000,
+        vinculo: 'CLT',
+    }
+]
+
+
+
+
+
+
+
