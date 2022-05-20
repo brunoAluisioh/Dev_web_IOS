5ce336f3cf4f64cd739b5ac17e1f6860e8212433
brunoAluisioh <brunoaluisioh@gmail.com>
2022-05-20 20:39:12 -0300

    Subindo exercício realizado Aula Dom_pt-2

---
+++ js/Aula 12_Dom_pt-2/JS/exercicio.js
@@ -0,0 +1,44 @@
+let btns = document.getElementsByClassName('B_12');
+btns[0].style.border = '2px solid #D9BC66';
+btns[0].style.padding = '.5rem';
+btns[0].style.backgroundColor = '#BFBFBF';
+btns[0].style.margin = '.7rem';
+
+btns[1].style.border = '2px solid #D9BC66';
+btns[1].style.padding = '.5rem';
+btns[1].style.backgroundColor = '#BFBFBF';
+btns[1].style.margin = '.7rem';
+
+btns[2].style.border = '2px solid #D9BC66';
+btns[2].style.padding = '.5rem';
+btns[2].style.backgroundColor = '#BFBFBF';
+btns[2].style.margin = '.7rem';
+
+const AlertCookie = () => {
+    alert('Imagem da Série Umbrella Academy')
+    let serie = document.createElement('serie'); //cria um elemento dentro do html
+    document.body.appendChild(serie); //(appenChild) = inclui um filho dentro de um elemento pai
+
+    let img = document.createElement('img');
+    img.src = './img/serie_Umbrella_Academy.png';
+    serie.appendChild(img);
+    document.body.style.backgroundColor = '#262525';
+}
+
+const Mensagem = () => {
+    let usuario = prompt ('Seja Bem-Vindo(a); por favor preencha seu nome:')
+
+    // comando innerHTML Faz aparecer na tela 
+    document.body.innerHTML = (`Olá ${usuario}, Bem-vindo a nossa academia.`);
+}
+
+const tabuada = () => {
+    let vezes = prompt ('Por favor preencher um número para multiplicação:');
+    
+    for(let contador = 1; contador <= 10; contador++) {
+        document.write(`${vezes} X ${contador} = ${vezes * contador} <br/>`);
+    }
+}
+
+
+
