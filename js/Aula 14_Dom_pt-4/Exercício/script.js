cb77d7da3139a99629ad84df06829b6c05aea050
brunoAluisioh <brunoaluisioh@gmail.com>
2022-05-25 21:49:48 -0300

    Subindo exercício realizado Aula Dom_pt-4

--- js/Aula 14_Dom_pt-4/Exercício/script.js
+++ js/Aula 14_Dom_pt-4/Exercício/script.js
@@ -8,26 +8,76 @@
 // const userList = document.querySelector('#users');
 
 var atualData = new Date(); // Obtém a data/hora atual
-var dia = data.getDate(); // 1-31
-var mes = data.getMonth(); // 0-11 (zero=janeiro)
-var ano4 = data.getFullYear(); // 4 dígitos
-var str_data = dia + '/' + (mes+1) + '/' + ano4; // Formata a data e a hora (note o mês + 1)
+var dia = atualData.getDate(); // 1-31
+var mes = atualData.getMonth(); // 0-11 (zero=janeiro)
+var ano4 = atualData.getFullYear(); // 4 dígitos
+var str_data = dia + '/' + (mes + 1) + '/' + ano4; // Formata a data e a hora (note o mês + 1)
 
 // informações HTML
 let nome = document.getElementById('name');
 let nasc = document.getElementById('nasc');
 let email = document.getElementById('email');
 let item = document.getElementById('item');
-let add = document.getElementById('add');
+let users = document.getElementById('users');
 
-// Configurando Botões 
-const addBtn = document.getElementById('adicionar'); //chamando pelo Id
+//adicionando valor nome, email, nascimento
+let form = document.getElementById('form');
+function adduser(e) {
+    e.preventDefault();
+    if (!validarIdade()) {
+        return;
+    }
+    if (!validandoEmail()) {
+        return;
+    }
+    var li = document.createElement('li');
+    li.innerText = `${nome.value}, ${nasc.value}, ${email.value}`;
+    users.appendChild(li);
+}
+
+function validarIdade() {
+    var nascimento = new Date(nasc.value);
+
+    //Calculando idade
+    let idade = atualData.getFullYear() - nascimento.getFullYear();
+    if (atualData.getMonth() < nascimento.getMonth()) {
+        idade--;
+    } else if (
+        atualData.getMonth() == nascimento.getMonth() &&
+        atualData.getDate() < nascimento.getDate() + 1
+    ) {
+        idade--;
+    }
+    // Validando idade
+    if (idade < 18) {
+        alert(`Somente +18`);
+        return false;
+    }
+    return true;
+}
+
+//Validando email
+function validandoEmail() {
+    let padrao = new RegExp(/.*@.*\..*/i);
+    padrao.test(email.value);
+    if (padrao.test(email.value)) {
+        return true;
+    }
+    alert(`Email inválido`);
+    return false;
+}
+
+//Excluir item
+function excluirItem() {
+    var usuarios = users.getElementsByTagName('li');
+    var valor = parseInt(item.value);
+    var li = usuarios[valor];
+    li.remove();
+    console.log(li);
+}
+
+// Configurando Botões
+form.addEventListener('submit', adduser);
+
 const delBtn = document.getElementById('excluir'); //chamando pelo Id
-addBtn.addEventListener('submit', onSubmit);
-delBtn.addEventListener('submit', onSubmit);
-
-
-
-
-
-
+delBtn.addEventListener('click', excluirItem);
