772706bd350d253304c82f9dd33101c21b540552
brunoAluisioh <brunoaluisioh@gmail.com>
2022-05-31 21:50:56 -0300

    Subindo Aula 15

--- js/Aula15_Dom_pt-5/Exercício/script.js
+++ js/Aula15_Dom_pt-5/Exercício/script.js
@@ -2,7 +2,6 @@
 const itens = [
 
 ]
-    
 
 //Chamando pelo Id
 let form = document.getElementById('addForm');
@@ -13,7 +12,7 @@ let filter = document.getElementById('filter');
 //Evento = adicionar item 
 form.addEventListener('submit', addItem);
 // Evento = remover item 
-itemList.addEventListener('click', removeItem);
+
 
 //Pra não carregar a página
 function addItem(e) {
@@ -22,7 +21,7 @@ function addItem(e) {
     let newText = document.getElementById('item-text').value;
     let newNumber = document.getElementById('item-number').value;
     let newPreco = document.getElementById('item-preco').value;
-
+    if (newNumber >0 && newPreco >0) {
     let descricao = `${newText} - Quantidade: ${newNumber} - Preço R$: ${newPreco}`
     let li = document.createElement('li');
     li.className = 'list-group-item';
@@ -44,47 +43,30 @@ function addItem(e) {
     let deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm m-1 float-end delete';
     deleteBtn.appendChild(document.createTextNode('X'));
+    deleteBtn.addEventListener('click', removeItem);
 
     li.appendChild(addBtn);
     li.appendChild(retirarBtn);
     li.appendChild(deleteBtn);
     itemList.appendChild(li);
-    form.reset(); 
 
-    itens.push(li)
-}
+    form.reset();
+    itens.push(li);
 
 // Função Remover Item
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-    let li = e.target.parentElement;
-    itemList.removeChild(li);
+function removeItem() {
+     itemList.removeChild(li)
 }
-}
-
 //Função diminuir item
 function diminuirItem() {
-    let li = this.parentElement
-    let descricao = `${li.nomeItem} - Quantidade: ${li.quantidadeItem -=1} - Preço R$: ${li.precoItem * li.quantidadeItem}`
+    let diminuir = newNumber -- 
+    let descricao = `${li.nomeItem} - Quantidade: ${li.quantidadeItem -=1} - Preço R$: ${li.precoItem / li.quantidadeItem}`
     li.innerText = descricao
-
-    let addBtn = document.createElement('button');
-    addBtn.className = 'btn btn-success btn-sm m-1 float-end adicionar';
-    addBtn.appendChild(document.createTextNode('+'));
-
-    let retirarBtn = document.createElement('button');
-    retirarBtn.addEventListener('click', diminuirItem)
-    retirarBtn.className = 'btn btn-primary btn-sm m-1 float-end retirar';
-    retirarBtn.appendChild(document.createTextNode('-'));
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn-sm m-1 float-end delete';
-    deleteBtn.appendChild(document.createTextNode('X'));
-
-    li.appendChild(addBtn);
-    li.appendChild(retirarBtn);
-    li.appendChild(deleteBtn);
+}
+}
 }
 
-//Função Aumentar
+else {
+    alert(`Insira uma quantidade maior que 0`);
+}
 
