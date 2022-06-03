16136b2806ebd5209004c49b20fc7c18e756eabd
brunoAluisioh <brunoaluisioh@gmail.com>
2022-06-02 21:21:40 -0300

    Subindo Aula 15 Dom_pt-5

--- js/Aula15_Dom_pt-5/Exercício/script.js
+++ js/Aula15_Dom_pt-5/Exercício/script.js
@@ -1,16 +1,17 @@
 //Criando um Array de objetos
-const itens = [
-]
+const itens = [];
 
 //Chamando pelo Id
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
 let valor = document.getElementById('preco');
+let total1 = document.getElementById('total');
 let filter = document.getElementById('filter');
+let soma = document.getElementById('soma');
 
-//Evento = adicionar item 
+//Evento = adicionar item
 form.addEventListener('submit', addItem);
-// Evento = remover item 
+// Evento = remover item
 
 //Pra não carregar a página
 function addItem(e) {
@@ -19,50 +20,101 @@ function addItem(e) {
     let newText = document.getElementById('item-text').value;
     let newNumber = document.getElementById('item-number').value;
     let newPreco = document.getElementById('item-preco').value;
-    if (newNumber >0 && newPreco >0) {
-    let descricao = `${newText} - Quantidade: ${newNumber} - Preço R$: ${newPreco * newNumber}`
-    let li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(descricao));
-    li.quantidadeItem = newNumber
-    li.nomeItem = newText
-    li.precoItem = newPreco
+    let valor = Number(newNumber * newPreco);
+    let lixo = -valor;
+    total(valor);
+    if (newNumber > 0 && newPreco > 0) {
+        let descricao = document.createTextNode(
+            `${newText} - Preço unitário R$: ${newPreco}`
+        );
 
-    let addBtn = document.createElement('button');
-    addBtn.className = 'btn btn-success btn-sm m-1 float-end adicionar';
-    addBtn.appendChild(document.createTextNode('+'));
+        let li = document.createElement('li');
+        li.className = 'list-group-item';
 
-    let retirarBtn = document.createElement('button');
-    retirarBtn.addEventListener('click', diminuirItem)
-    retirarBtn.className = 'btn btn-primary btn-sm m-1 float-end retirar';
-    retirarBtn.appendChild(document.createTextNode('-'));
+        li.nomeItem = newText;
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn-sm m-1 float-end delete';
-    deleteBtn.appendChild(document.createTextNode('X'));
-    deleteBtn.addEventListener('click', removeItem);
+        let addBtn = document.createElement('button');
+        addBtn.className = 'btn btn-success btn-sm m-1 float-end adicionar';
+        addBtn.appendChild(document.createTextNode('+'));
 
-    li.appendChild(addBtn);
-    li.appendChild(retirarBtn);
-    li.appendChild(deleteBtn);
-    itemList.appendChild(li);
+        let retirarBtn = document.createElement('button');
+        retirarBtn.addEventListener('click', diminuirItem);
+        retirarBtn.className = 'btn btn-primary btn-sm m-1 float-end retirar';
+        retirarBtn.appendChild(document.createTextNode('-'));
 
-    form.reset();
-    itens.push(li);
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className = 'btn btn-danger btn-sm m-1 float-end delete';
+        deleteBtn.appendChild(document.createTextNode('X'));
+        deleteBtn.addEventListener('click', removeItem);
 
-// Função Remover Item
-function removeItem() {
-     itemList.removeChild(li)
+        li.appendChild(descricao);
+        li.appendChild(addBtn);
+        li.appendChild(retirarBtn);
+        li.appendChild(deleteBtn);
+        itemList.appendChild(li);
+        let li2 = document.createElement('li');
+        li2.className = 'list-group-item';
+        li2.appendChild(
+            document.createTextNode(
+                `${li.nomeItem} - Quantidade: ${newNumber} --> Preço R$: ${
+                    newPreco * newNumber
+                }`
+            )
+        );
+        total1.appendChild(li2);
+
+        // Função Remover Item
+        function removeItem() {
+            itemList.removeChild(li);
+            total1.removeChild(li2);
+            total(lixo);
+        }
+
+        function adicionar() {
+            if (newNumber <= 100) {
+                var add = +newPreco;
+                let adicionar = newNumber++;
+                var novoPreco2 = newPreco * adicionar;
+                li2.innerText = `${li.nomeItem} - Quantidade: ${
+                    adicionar + 1
+                } --> Preço R$: ${novoPreco2 + add}`;
+                lixo = -novoPreco2 - newPreco;
+                total(add);
+            }
+        }
+        addBtn.addEventListener('click', adicionar);
+        //Função diminuir item
+        function diminuirItem() {
+            if (newNumber >= 1) {
+                var dec = -newPreco;
+                let diminuir = newNumber--;
+                var novoPreco = newPreco * diminuir;
+                li2.innerText = `${li.nomeItem} - Quantidade: ${
+                    diminuir - 1
+                } --> Preço R$: ${(novoPreco -= newPreco)}`;
+                lixo = -novoPreco;
+                total(dec);
+            }
+        }
+
+        form.reset();
+    } else if (newNumber <= 0 || newPreco <= 0) {
+        alert(`Insira uma quantidade maior que 0`);
+    } else {
+        alert(`Insira uma quantidade maior que 0`);
+    }
 }
-//Função diminuir item
-function diminuirItem() {
-    let diminuir = newNumber -- 
-    let descricao = `${li.nomeItem} - Quantidade: ${diminuir} - Preço R$: ${li.precoItem * diminuir}`
-    li.innerText = descricao
-}
 
-}
-else {
-    alert(`Insira uma quantidade maior que 0`);
-}
+function total(i) {
+    let inicial = 0;
+    let price = parseInt(i);
+    itens.push(price);
+    if (itens.length <= 1) {
+        inicial = price;
+    } else {
+        for (i = 0; i < itens.length; i++) {
+            inicial += itens[i];
+        }
+    }
+    soma.innerHTML = `R$: ${inicial},00`;
 }
