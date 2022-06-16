c075c8a7f60c1fcf893ccef72a78716677ade9a8
brunoAluisioh <brunoaluisioh@gmail.com>
2022-06-16 18:25:25 -0300

    Subindo exercício realizado 02 Java

---
+++ java/02_TiposOperadores/exercicio_02/CalcArea/src/CalcArea.java
@@ -0,0 +1,8 @@
+public class CalcArea {
+    public static void main (String[] args) {
+        int base = 8;
+        int alt = 4;
+        System.out.println("A área do triângulo retângulo é de:" + (base * alt)/2);
+        
+    }
+}
---
+++ java/02_TiposOperadores/exercicio_02/CalcMedia/src/CalcMedia.java
@@ -0,0 +1,9 @@
+public class CalcMedia {
+    public static void main(String[] args) {
+        double nota1 = 6.0;
+        double nota2 = 7.5;
+        double nota3 = 9.3;
+        double nota4 = 5.4;
+        System.out.println("A média da turma é de:" + (nota1 + nota2 + nota3 + nota4) / 4);
+    }
+}
