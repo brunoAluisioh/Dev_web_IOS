471b99570fad3aecacc7e7b8d0e672051638748a
unknown <brunoaluisioh@gmail.comgit config --global user.name brunoAluisioh>
2022-06-29 21:52:33 -0300

    Subindo Aula 09 Java

--- java/09_String/Exercicio/src/Strings.java
+++ java/09_String/Exercicio/src/Strings.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
-
 public class Strings {
     public static void main (String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        String frutas =
+        String[]frutas = new String[5];
+
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.println("Digite o nome de uma fruta: " );
+            frutas[i] = teclado.nextLine();
+          ,
+        }
+
     }
 }
