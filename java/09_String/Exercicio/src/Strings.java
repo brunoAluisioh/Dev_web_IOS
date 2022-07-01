d13758c76a199a6a1ccb6ab257b1c9c785777718
brunoAluisioh <brunoaluisioh@gmail.com>
2022-06-30 21:31:08 -0300

    Subindo exercício realizado Aula 09 Java

--- java/09_String/Exercicio/src/Strings.java
+++ java/09_String/Exercicio/src/Strings.java
@@ -1,15 +1,37 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class Strings {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        String[]frutas = new String[5];
+        String[] frutas = new String[5];
 
-        for (int i = 0; i < frutas.length; i++) {
-            System.out.println("Digite o nome de uma fruta: " );
-            frutas[i] = teclado.nextLine();
-          ,
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite o nome de uma fruta " + (i + 1) + " :");
+            String vazia = teclado.nextLine();
+            boolean add = true;
+
+            for (int j = 0; j < 5; j++) {
+                if (frutas[j] == null) {
+                    continue;
+                }
+                if (vazia.compareTo(frutas[j]) == 0 || vazia.isEmpty() || vazia.length() < 3) {
+                    System.out.println("Insira um nome válido");
+                    add = false;
+                    break;
+                }
+            }
+            if (add) {
+                frutas[i] = vazia;
+            }
         }
-
+        
+        for (int i = 0; i < frutas.length; i++) {
+            if (frutas[i] != null) {
+                System.out.print(frutas[i].toUpperCase() + " ");
+                System.out.print(frutas[i].toLowerCase() + " ");
+                System.out.println(" ");
+            }
+        }
     }
 }
