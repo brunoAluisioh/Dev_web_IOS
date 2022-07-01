40417e516e7ff2fe8c212d74c9265c2290a4e2b3
brunoAluisioh <brunoaluisioh@gmail.com>
2022-06-30 21:32:23 -0300

    Subindo Aula 10 Java

--- java/10_Matriz/Projeto_1_Matriz/src/Exemplo1Matriz.java
+++ java/10_Matriz/Projeto_1_Matriz/src/Exemplo1Matriz.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Exemplo1Matriz {
+public class Exemplo1Matriz {
+    public static void main(String[] args) {
+
+        // Declarar e inicializar a matriz
+        // 3 linhas 4 colunas
+        int numeros[][] = {{4, 5, 1, 10}, {16, 11, 76, 8}, {9, 54, 32, 89}};
+
+        int i, j;
+
+        for (i = 0; i < numeros.length; i++) {
+            for (j = 0; j < numeros[i].length; j++) {
+                System.out.print(numeros[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+    }
 }
